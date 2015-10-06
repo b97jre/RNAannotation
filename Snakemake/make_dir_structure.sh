615e4b635cdc4bed16a6abdade99d9d65a41a36e
Tasos <glaros.t@gmail.com>
2015-10-06 15:46:30 +0200

    Create make_dir_structure.sh

---
+++ Snakemake/make_dir_structure.sh
@@ -0,0 +1,8 @@
+# Use this script to create the folder structure, if you do not have it already.
+
+mkdir train_set
+mkdir train_set/IDX
+mkdir train_set/BowtieRef
+mkdir train_set/intermediate_files
+mkdir train_set/intermediate_files/orf_pfam
+mkdir train_set/intermediate_files/expression
