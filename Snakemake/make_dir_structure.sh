79a4bf1cdbd0f6f85993a558fd97c0b04cf6cff4
Tasos <glaros.t@gmail.com>
2015-10-06 16:07:35 +0200

    Update make_dir_structure.sh

--- Snakemake/make_dir_structure.sh
+++ Snakemake/make_dir_structure.sh
@@ -1,8 +1,8 @@
 # Use this script to create the folder structure, if you do not have it already.
 
-mkdir train_set
-mkdir train_set/IDX
-mkdir train_set/BowtieRef
-mkdir train_set/intermediate_files
-mkdir train_set/intermediate_files/orf_pfam
-mkdir train_set/intermediate_files/expression
+mkdir main_dir
+mkdir main_dir/IDX
+mkdir main_dir/BowtieRef
+mkdir main_dir/intermediate_files
+mkdir main_dir/intermediate_files/orf_pfam
+mkdir main_dir/intermediate_files/expression
